f3ae9c7c799c2d8316a8fc3a1736e1d00ebf020c
Karen Xie <karen.xie@myob.com>
2018-01-23 09:27:12 +1300

    clean up

--- lib/phone_number_kata.ex
+++ lib/phone_number_kata.ex
@@ -1,27 +1,18 @@
 defmodule PhoneNumberKata do
-  @moduledoc """
-  Documentation for PhoneNumberKata.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PhoneNumberKata.hello
-      :world
-
-  """
-  def isConsistent(namesAndNumbers) do
-    listOfNumbers = extractNumbers(namesAndNumbers)
-    Enum.all?(listOfNumbers, fn(number) -> 
-      !isPrefix(number, listOfNumbers -- [number]) 
-    end)
+  def verify(namesAndNumbers) do
+    list_of_numbers = extractNumbers(namesAndNumbers)
+    isConsistent(list_of_numbers)
   end
 
   def extractNumbers(namesAndNumbers) do
     lineBreakSeparatedNumbers = String.replace(namesAndNumbers, ~r/[^0-9\n]/, "")
     String.split(lineBreakSeparatedNumbers, "\n\n")
+  end
+
+  def isConsistent(listOfNumbers) do
+    Enum.all?(listOfNumbers, fn(number) -> 
+      !isPrefix(number, listOfNumbers -- [number]) 
+    end)
   end
 
   def isPrefix(number, list) do
--- test/phone_number_kata_test.exs
+++ test/phone_number_kata_test.exs
@@ -1,20 +1,28 @@
 defmodule PhoneNumberKataTest do
   use ExUnit.Case
 
+  test "verify input" do
+    assert PhoneNumberKata.verify(
+      "Bob 91 12 54 26\n
+      Alice 97 625 992\n  
+      Emergency 911"
+      ) == false
+  end
+
+  test "verify input 2" do
+    assert PhoneNumberKata.verify(
+      "Bob,91-12-54 26\n
+      Alice,97 625-992\n  
+      Emergency 911"
+      ) == false
+  end
+
   test "is not consistent" do
-    assert PhoneNumberKata.isConsistent(
-    "Bob 91 12 54 26\n
-    Alice 97 625 992\n  
-    Emergency 911"
-    ) == false
+    assert PhoneNumberKata.isConsistent(["91125426", "97625992", "911"]) == false
   end
 
   test "is consistent" do
-    assert PhoneNumberKata.isConsistent(
-    "Bob 91 12 54 26\n
-    Alice 97 625 921\n  
-    Emergency 921"
-    ) == true
+    assert PhoneNumberKata.isConsistent(["91125426", "97625921", "921"]) == true
   end
 
   test "convert names and numbers to list of numbers without whitespace 1" do
@@ -27,8 +35,8 @@ defmodule PhoneNumberKataTest do
 
   test "convert names and numbers to list of numbers without whitespace 2" do
     assert PhoneNumberKata.extractNumbers(
-      "Mark Pearl 123 456\n
-      Darren Sim 111 333\n  
+      "Mark Pearl 123-456\n
+      Darren Sim 111-333\n  
       Grads 111"
       ) == ["123456", "111333", "111"]
   end
