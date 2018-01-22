23fb5e27b7ab3d3ce977d2b4926128cbc937dda4
Karen Xie <karen-x@outlook.com>
2018-01-23 08:23:46 +1300

    finished i think?

--- lib/phone_number_kata.ex
+++ lib/phone_number_kata.ex
@@ -12,11 +12,21 @@ defmodule PhoneNumberKata do
       :world
 
   """
-  def isConsistent(numbers) do
-    Enum.all?(numbers, fn(s) -> !isPrefix(s, numbers) end)
+  def isConsistent(namesAndNumbers) do
+    listOfNumbers = extractNumbers(namesAndNumbers)
+    Enum.all?(listOfNumbers, fn(number) -> 
+      !isPrefix(number, listOfNumbers -- [number]) 
+    end)
+  end
+
+  def extractNumbers(namesAndNumbers) do
+    lineBreakSeparatedNumbers = String.replace(namesAndNumbers, ~r/[^0-9\n]/, "")
+    String.split(lineBreakSeparatedNumbers, "\n\n")
   end
 
   def isPrefix(number, list) do
-    Enum.any?(list, fn(s) -> String.starts_with?(s, number) end)
+    Enum.any?(list, fn(n) -> 
+      String.starts_with?(n, number) 
+    end)
   end
 end
--- test/phone_number_kata_test.exs
+++ test/phone_number_kata_test.exs
@@ -1,19 +1,43 @@
 defmodule PhoneNumberKataTest do
   use ExUnit.Case
 
-  test "is a prefix in list" do
-    assert PhoneNumberKata.isPrefix("123", ["123456", "456"]) == true
-  end
-
-  test "is not prefix in list 1" do
-    assert PhoneNumberKata.isPrefix("123", ["456123", "456"]) == false
-  end
-
-  test "is not a prefix in list 2" do
-    assert PhoneNumberKata.isPrefix("123", ["456", "789"]) == false
-  end
-
   test "is not consistent" do
-    assert PhoneNumberKata.isConsistent(["91 12 54 26", "97 625 992", "911"]) == false
+    assert PhoneNumberKata.isConsistent(
+    "Bob 91 12 54 26\n
+    Alice 97 625 992\n  
+    Emergency 911"
+    ) == false
+  end
+
+  test "is consistent" do
+    assert PhoneNumberKata.isConsistent(
+    "Bob 91 12 54 26\n
+    Alice 97 625 921\n  
+    Emergency 921"
+    ) == true
+  end
+
+  test "convert names and numbers to list of numbers without whitespace 1" do
+    assert PhoneNumberKata.extractNumbers(
+      "Bob 91 12 54 26\n
+      Alice 97 625 992\n  
+      Emergency 911"
+      ) == ["91125426", "97625992", "911"]
+  end
+
+  test "convert names and numbers to list of numbers without whitespace 2" do
+    assert PhoneNumberKata.extractNumbers(
+      "Mark Pearl 123 456\n
+      Darren Sim 111 333\n  
+      Grads 111"
+      ) == ["123456", "111333", "111"]
+  end
+
+  test "is a prefix of item in list" do
+    assert PhoneNumberKata.isPrefix("111", ["123456", "111333"]) == true
+  end
+
+  test "is not a prefix of item in list" do
+    assert PhoneNumberKata.isPrefix("111", ["123456", "444333"]) == false
   end
 end
